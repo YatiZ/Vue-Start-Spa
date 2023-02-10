05bfd9278a153fa2b8a020a657a909d1e16ebe3c
jeffery <jeffery.arkar@weborigo.eu>
2023-02-10 19:30:52 +0630

    bug fixed

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,48 @@
 <template>
-  <nav-bar-view :pages="pages" :activePage="activePage" :nav-link-click="(index) => activePage = index"></nav-bar-view>
-  
+  <nav-bar-view
+    :pages="pages"
+    :activePage="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+    @change-active-page="changeActivePage"
+  ></nav-bar-view>
+
   <page-view :page="pages[activePage]"></page-view>
   <h1>Hello World</h1>
 </template>
 
 <script>
-import PageView from './components/PageView.vue';
-import NavBarView from './components/NavBar.vue';
-export default{
-  components:{NavBarView,PageView},
+import PageView from "./components/PageView.vue";
+import NavBarView from "./components/NavBar.vue";
+export default {
+  components: { NavBarView, PageView },
   data() {
-                return {
-                    theme1: '',
-                    useDarkNav: true,
-                    activePage:0,
-                    pages:[
-                        {
-                            link: {text:'Home',url:'home.html'},
-                            pageTitle:'Home Page',
-                            content:'This is Home page'
-                        },
-                        {
-                            link: {text:'About',url:'about.html'},
-                            pageTitle:'About Page',
-                            content:'This is about page'
-                        },
-                        {
-                            link: {text:'contact',url:'contact.html'},
-                            pageTitle:'Contact Page',
-                            content:'This is contact page'
-                        },
-                        
-                    ]
-                }
-            },
-}
+    return {
+      theme1: "",
+      useDarkNav: true,
+      activePage: 0,
+      pages: [
+        {
+          link: { text: "Home", url: "home.html" },
+          pageTitle: "Home Page",
+          content: "This is Home page",
+        },
+        {
+          link: { text: "About", url: "about.html" },
+          pageTitle: "About Page",
+          content: "This is about page",
+        },
+        {
+          link: { text: "contact", url: "contact.html" },
+          pageTitle: "Contact Page",
+          content: "This is contact page",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeActivePage(index) {
+      this.activePage = index;
+    },
+  },
+};
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,36 +1,46 @@
 <template>
-    <nav class="navbar navbar-expand-lg"
-     :class="{'navbar-success bg-success': !useDarkNav,'navbar-dark bg-dark': useDarkNav}"
-    >
+  <nav
+    class="navbar navbar-expand-lg"
+    :class="{
+      'navbar-success bg-success': !useDarkNav,
+      'navbar-dark bg-dark': useDarkNav,
+    }"
+  >
+    <a class="navbar-brand mx-4" href="">Navbar</a>
 
-        <a class="navbar-brand mx-4 " href="">Navbar</a>
-        
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav" v-for="(page,index) in pages" :key="index">
-            <a class="nav-item nav-link"
-             :class="{active:activePage == index}"
-             :href="page.link.url"
-             :title ="`This link goes to ${page.link.text} page`"
-             @click.prevent="navLinkClick(index)"
-             >{{page.link.text}}</a>
-            
-          </div>
-        </div>
-        <form class="d-flex">
-            <button class="btn btn-primary" @click.prevent="useDarkNav = !useDarkNav">Change Nav color</button>
-        </form>
-      </nav>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav" v-for="(page, index) in pages" :key="index">
+        <a
+          class="nav-item nav-link"
+          :class="{ active: activePage == index }"
+          :href="page.link.url"
+          :title="`This link goes to ${page.link.text} page`"
+          @click.prevent="navLinkClick(page, index)"
+          >{{ page.link.text }}</a
+        >
+      </div>
+    </div>
+    <form class="d-flex">
+      <button class="btn btn-primary" @click.prevent="useDarkNav = !useDarkNav">
+        Change Nav color
+      </button>
+    </form>
+  </nav>
 </template>
 <script>
-export default{
-    
-    props:['pages','activePage','NavLinkClick'],
-    data(){
-        return{
-            
-            useDarkNav: true, 
-        }
+export default {
+  props: ["pages", "activePage", "NavLinkClick"],
+  data() {
+    return {
+      useDarkNav: true,
+    };
+  },
+  methods: {
+    navLinkClick(page, index) {
+      window.history.pushState({}, "", page.link.url);
+
+      this.$emit("change-active-page", index);
     },
-    
-}
+  },
+};
 </script>
